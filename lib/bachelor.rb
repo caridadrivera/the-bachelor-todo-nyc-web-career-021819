5d430da110e7050ea7d3e03c2344db7c5429969b
lively-teacher-9822 <ccrive10@gmail.com>
2019-02-01 17:48:47 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -10,7 +10,11 @@ end
 
 
 def get_contestant_name(data, occupation)
-
+  data["season"].each do |value|
+    if value["occupation"] == occupation
+       value["name"]
+      end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)

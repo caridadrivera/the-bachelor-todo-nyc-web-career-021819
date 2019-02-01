cf5f7a4179bce53ae99501429e42c5d1785aa6ff
lively-teacher-9822 <ccrive10@gmail.com>
2019-02-01 18:23:29 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -40,9 +40,19 @@ end
 
 
 
-def get_occupation(data, hometown)
+def count_contestants_by_hometown(data, hometown)
   # code here
+  counter = 0
+  data.each do |season, contestants|
+    contestants.each do |contestant|
+      if contestant["hometown"] == hometown
+        counter += 1
+      end
+    end
+  end
+  counter
 end
+
 
 def get_average_age_for_season(data, season)
   # code here

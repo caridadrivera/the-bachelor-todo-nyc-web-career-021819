acb56345f15bffcc9dee27a6e6973c21aaefc09c
lively-teacher-9822 <ccrive10@gmail.com>
2019-02-01 18:24:08 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -41,10 +41,9 @@ end
 
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
-  counter = 0
-  data.each do |season, contestants|
-    contestants.each do |contestant|
+counter = 0
+  data.each do |season, person|
+    person.each do |contestant|
       if contestant["hometown"] == hometown
         counter += 1
       end

b20773925f1a245e0f7d80426f20cb2d7470788b
lively-teacher-9822 <ccrive10@gmail.com>
2019-02-01 18:03:10 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,18 +1,18 @@
 def get_first_name_of_season_winner(data, season)
   #the hash is called data and we know that the seasons are in the first layer of the data hash and we want to access those seasons to iterate over them.
   
-  data["season"].each do |value|  
-    if value["status"] == "Winner"
-      value["name"].first
+  data[season].each do |value|  
+    if value[status] == "Winner"
+      value[name].first
     end
   end
 end
 
 
 def get_contestant_name(data, occupation)
-  data["season"].each do |value|
-    if value["occupation"] == occupation
-       value["name"]
+  data[season].each do |value|
+    if value[occupation] == occupation
+       value[name]
     end
   end
 end
